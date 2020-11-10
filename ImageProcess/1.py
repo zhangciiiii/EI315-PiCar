f3a4bf1ddb4288ab2899f546e080b0d175406aba
zhangciiiii <zhangciiiii@github.com>
2020-11-10 14:37:30 +0800

    image process

---
+++ ImageProcess/1.py
@@ -0,0 +1,55 @@
+
+import math
+import cv2
+import numpy as np
+import os
+import matplotlib.pyplot as plt
+
+import copy
+
+
+def cv_imread(file_path):
+    cv_img = cv2.imdecode(np.fromfile(file_path,dtype=np.uint8),-1)
+    return cv_img
+
+
+
+img = cv_imread('straight.jpg') # straight left right
+
+img = img[:150,:,:] #150,640
+
+img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+cv2.circle(img,(240,-80),150,255,-1)
+cv2.imwrite('straight_gray.jpg'  , img)
+
+_, img = cv2.threshold(img, 100, 255, cv2.THRESH_BINARY)
+
+kernel = np.ones((3, 3), np.uint8)
+img = cv2.dilate(img,kernel,iterations=3)
+
+mask = img.copy()
+mask[mask<100]=1
+mask[mask>=100]=0
+
+lines = cv2.HoughLines(mask,1,np.pi/180,100) 
+result = img.copy()
+
+
+lines = lines[:,0,1]
+
+left_cnt = copy.deepcopy(lines)
+right_cnt = copy.deepcopy(lines)
+left_cnt[left_cnt<1.5]=0
+left_cnt[left_cnt>1.5]=1
+left_cnt = left_cnt.sum()
+
+right_cnt[right_cnt<1.5]=1
+right_cnt[right_cnt>1.5]=0
+right_cnt = right_cnt.sum()
+
+
+print(left_cnt,right_cnt)
+cv2.imwrite('straight_binary.jpg'  , img)
+
+       
--- run_picar.py
+++ run_picar.py
@@ -11,83 +11,21 @@ def run_picar():
     my_driver.setStatus(motor=0.0, servo=0.0, dist=0x00, mode="stop")
     tmp_speed = 1
     Speed_Control_Flag = True
+  
 
-    # my_driver.setStatus(...)
-    # Motor control:
-    # motor = ...
-    # forward  0 -> 1.0
-    # backward 0 -> -1.0
-                
-    # Servo control:
-    # servo = ...
-    # left = mid = right
-    # 1.0  = 0.0 =  -1.0
-
-    # Distance:
-    # dist = ...
-    # 0 -> 0xffffffff
-    
-    # Mode control:
-    # 1:speed
-    # 2:distance
-    # 3:stop
-
-    # my_driver.getStatus(...)
-    # query mode:
-    # mode = any
-    # sensor = any   
-    ''' 
-    try:
-        print("Test 1/2: speed mode")
-        my_driver.setStatus(mode="speed")
-        while True:
-            st = tmp_speed * 0.1 -1
-            sm = st
-            if abs(st) < 0.4:
-                sm = 0.4 if st > 0 else -0.4
-                if st == 0:
-                    sm = 0
-            my_driver.setStatus(motor = sm, servo = st)
-            print("Motor: %0.2f, Servo: %0.2f" % (sm, st))
-            if Speed_Control_Flag:
-                tmp_speed += 1
-            else:
-                tmp_speed -= 1
-            if tmp_speed > 20:
-                Speed_Control_Flag = False
-                tmp_speed -= 2
-            if tmp_speed < 0:
-                Speed_Control_Flag = True
-                tmp_speed += 2
-            time.sleep(1)
-            # my_driver.heartBeat()
-            my_driver.getStatus(sensor = 0, mode = 0)
-            time.sleep(1)
-    except KeyboardInterrupt:
-        pass
-    '''
     try:
         print("Test 2/2: distance mode")
-        my_driver.setStatus(motor = 0.5, dist = 0x1ff00, mode="distance")
+        my_driver.setStatus(motor = 0.1, dist = 0x1ff00, mode="distance")
         while True:
-            speed_param = tmp_speed * 0.1 - 1
+            cap2 = cv2.VideoCapture(1)
+            _, frame2 = cap2.read()
+            cv2.imshow("image2", frame2)
+            speed_param = 0
             my_driver.setStatus(servo = speed_param)
             print("Servo: %0.2f" % speed_param)
-            '''
-            if Speed_Control_Flag:
-                tmp_speed += 1
-            else:
-                tmp_speed -= 1
-            if tmp_speed > 20:
-                Speed_Control_Flag = False
-                tmp_speed -= 2
-            if tmp_speed < 0:
-                Speed_Control_Flag = True
-                tmp_speed += 2
-            '''
-            time.sleep(1)
+
             my_driver.getStatus(sensor = 0, mode = 0)
-            time.sleep(1)
+
     except KeyboardInterrupt:
         pass
     my_driver.setStatus(motor=0.0, servo=0.0, dist=0x00, mode="stop")
